1e5886a7de1adc74b8152999ed68220a59fff798
Kevin Stough <kstough@gmail.com>
2020-04-13 19:45:09 -0400

    Testing maven release plugin

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,9 @@
 package sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
