dab9dece7f7ae19b04deeecc5279fdc25d928c9c
Kevin Stough <kstough@gmail.com>
2020-04-18 11:51:01 -0400

    Implement map based services.  Closes #6.

---
+++ pet-clinic-data/src/main/java/com/kevin/sfgpetclinic/services/map/PersonServiceMap.java
@@ -0,0 +1,34 @@
+package com.kevin.sfgpetclinic.services.map;
+
+import com.kevin.sfgpetclinic.model.Person;
+import com.kevin.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PersonServiceMap extends AbstractMapService<Person, Long> implements CrudService<Person, Long> {
+
+    @Override
+    public Set<Person> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Person object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Person save(Person object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Person findById(Long id) {
+        return super.findById(id);
+    }
+}
