e56057ae88b9d2b8a6ddca17b0d2124bdd1b835f
Kevin Stough <kstough@gmail.com>
2020-04-28 22:27:29 -0400

    Added owners list and updated controller.  Closes #16.

--- pet-clinic-web/src/main/java/com/kevin/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/kevin/sfgpetclinic/controllers/PetController.java
@@ -4,7 +4,7 @@ package com.kevin.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/owners")
+@RequestMapping("/pets")
 @Controller
 public class PetController {
     @RequestMapping({"", "/", "/index", "/index.html"})
