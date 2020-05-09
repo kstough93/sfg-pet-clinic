bc318e08281c897c4fafdcad0cc17c7eaa342b18
Kevin Stough <kstough@gmail.com>
2020-05-09 11:13:29 -0400

    Added Specialty, associated to Vet. Closes #27.

--- pet-clinic-data/src/main/java/com/kevin/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/kevin/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package com.kevin.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialty;
+
+    public Set<Specialty> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Set<Specialty> specialty) {
+        this.specialty = specialty;
+    }
 }
