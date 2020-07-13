09d7f0600d15efc080f5a1ed99ebe56c3f8a80d5
Kevin Stough <kstough@gmail.com>
2020-07-13 18:36:08 -0400

    Add Visit JPA Service.  Closes #47.

--- pet-clinic-web/src/main/java/com/kevin/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kevin/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.kevin.sfgpetclinic.bootstrap;
 
 import com.kevin.sfgpetclinic.model.*;
-import com.kevin.sfgpetclinic.services.OwnerService;
-import com.kevin.sfgpetclinic.services.PetTypeService;
-import com.kevin.sfgpetclinic.services.SpecialtyService;
-import com.kevin.sfgpetclinic.services.VetService;
+import com.kevin.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -71,7 +70,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasPet);
         ownerService.save(owner2);
 
-        System.out.println("Loaded Owners and Pets... ");
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy kitty.");
+        visitService.save(catVisit);
+
+        System.out.println("Loaded Owners, Pets, and Visits... ");
 
         Specialty radiology = new Specialty();
         radiology.setDescription("Radiology");
