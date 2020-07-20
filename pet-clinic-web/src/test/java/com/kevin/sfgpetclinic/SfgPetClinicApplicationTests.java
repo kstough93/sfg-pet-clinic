47e6020189ab2ee2a72517a933d3c141c273f91a
Kevin Stough <kstough@gmail.com>
2020-07-19 22:13:52 -0400

    Upgrade to Junit 5.  Closes #4.

---
+++ pet-clinic-web/src/test/java/com/kevin/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kevin.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
