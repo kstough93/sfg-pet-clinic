d1651779187de5cfcdb73358a8d611b2c47edff4
Kevin Stough <kstough@gmail.com>
2021-02-23 20:11:21 -0500

    Adding new Owner views.

--- pet-clinic-web/src/main/java/com/kevin/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/kevin/sfgpetclinic/controllers/OwnerController.java
@@ -1,9 +1,13 @@
 package com.kevin.sfgpetclinic.controllers;
 
+import com.kevin.sfgpetclinic.model.Owner;
 import com.kevin.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -18,10 +22,19 @@ public class OwnerController {
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
-    };
+    }
 
     @RequestMapping("/find")
     public String findOwners() {
         return "notimplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwners(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
+    }
+
 }
